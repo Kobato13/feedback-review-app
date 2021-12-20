42e78d188bdca3ea75f51beaeda581d6dfee029a
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-21 01:34:59 +0800

    Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # Returns a full title on a per page basis
+
+  # Method definition, optional argument
+  def full_title(page_title = ' ')
+    base_title = "Orbit Feedback Application" # Creating and assigning a variable
+    # Boolean test
+    if page_title.empty?
+      base_title # Implicit return
+    else
+      page_title + " | " + base_title #  String concatenation
+    end
+  end
 end
