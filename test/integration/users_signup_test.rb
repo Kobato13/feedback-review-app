e644e321028d6a32897a1609f139869181f2174f
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-22 08:02:07 +0800

    Complete user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do 
+      post users_path, params: { user: 
+        {  name: " ", email: "tony@example", password: "pass",
+           password_confirmation: "word" }}
+      end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do 
+      post users_path, params: { user: 
+        {  name: "John Carlo Martinez",
+           email: "johncarlomartinez@example.com",
+           password: "123123",
+           password_confirmation: "123123" }}
+      end
+      follow_redirect!
+      assert_template 'users/show'
+  end
+end
