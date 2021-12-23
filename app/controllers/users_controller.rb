a4a1cf83627a86b3956847221919f542dab3e276
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-23 08:27:18 +0800

    Displaying snapshots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @snapshots = @user.snapshots.paginate(page: params[:page])
   end
 
   def create
