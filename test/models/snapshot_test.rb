888bb9095d3d91253d3b9fcafbd4ce19f9b4e357
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-23 08:11:55 +0800

    Adding user snapshot association + tests

--- test/models/snapshot_test.rb
+++ test/models/snapshot_test.rb
@@ -4,6 +4,7 @@ class SnapshotTest < ActiveSupport::TestCase
   def setup
     @user = users(:tony)
     @snapshot = @user.snapshots.build(content: "test content")
+    # @snapshot = Snapshot.new(content: "test content", user_id: @user.id)
   end
 
   test "should be valid" do
@@ -12,6 +13,16 @@ class SnapshotTest < ActiveSupport::TestCase
 
   test "user id should be present" do
     @snapshot.user_id = nil
+    assert_not @snapshot.valid?
+  end
+
+  test "content should be present" do
+    @snapshot.content = "    "
+    assert_not @snapshot.valid?
+  end
+
+  test "content should ne no more than 250 characters" do
+    @snapshot.content = "a" * 251
     assert_not @snapshot.valid?
   end
 end
